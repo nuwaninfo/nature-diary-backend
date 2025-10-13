19a23707c9043f07dc897401b5ba02e3b1dd081b
OviniP <ovinipoornima@gmail.com>
2025-10-13 06:42:45 +0300

    chore: Created data source and made the connection

--- server.ts
+++ server.ts
@@ -1,22 +1,29 @@
-import express from "express"
-import type { Express } from "express"
-import morgan from "morgan"
-import router from "./src/index.js"
-import dotenv from "dotenv"
+import express from "express";
+import type { Express } from "express";
+import morgan from "morgan";
+import router from "./src/index.js";
+import dotenv from "dotenv";
+import { AppDataSource } from "./src/data-source.js";
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
-const port: number = parseInt(process.env.PORT as string) || 8001
+const app: Express = express();
+const port: number = parseInt(process.env.PORT as string) || 8001;
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Database connection success");
+  })
+  .catch((err) => console.log(err));
 
 // Middleware in Express that allows your server to parse incoming requests with JSON payloads.
-app.use(express.json())
+app.use(express.json());
 
 // Middleware to parse URL-encoded bodies
-app.use(express.urlencoded({extended: false}))
-app.use(morgan("dev"))
-app.use("/", router)
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use("/", router);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entities/User.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 5433,
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASS || "abc123",
+  database: process.env.DB_NAME || "nature-diary",
+  synchronize: false,
+  logging: true,
+  entities: [User],
+  migrations: ["src/migrations/*.ts"],
+});
